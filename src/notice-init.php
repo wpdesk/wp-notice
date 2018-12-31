e0d7ba11f87dcdd1530794aacd00fa8699e48531
Grzegorz Rola <grola@seostudio.pl>
2018-12-31 10:41:14 +0100

    notice-init

--- src/notice-init.php
+++ src/notice-init.php
@@ -1,7 +1,15 @@
 <?php
 
-require_once './WPDesk/Notice/AjaxHandler.php';
-require_once './WPDesk/Notice/Notice.php';
-require_once './WPDesk/Notice/PermanentDismissibleNotice.php';
-require_once './WPDesk/Notice/Factory.php';
-require_once './WPDesk/Notice/functions.php';
+if (!class_exists('\WPDesk\Notice\AjaxHandler')) {
+    require_once './WPDesk/Notice/AjaxHandler.php';
+}
+if (!class_exists('\WPDesk\Notice\Notice')) {
+    require_once './WPDesk/Notice/Notice.php';
+}
+if (!class_exists('\WPDesk\Notice\PermanentDismissibleNotice')) {
+    require_once './WPDesk/Notice/PermanentDismissibleNotice.php';
+}
+if (!class_exists('\WPDesk\Notice\Factory')) {
+    require_once './WPDesk/Notice/Factory.php';
+}
+require_once './WPDesk/notice-functions.php';
