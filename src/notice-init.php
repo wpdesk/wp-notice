723bde2f03d0ad9537b788666371716936144a41
Grzegorz Rola <grola@seostudio.pl>
2018-12-31 11:08:40 +0000

    Feature/init

--- src/notice-init.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once './WPDesk/Notice/AjaxHandler.php';
-require_once './WPDesk/Notice/Notice.php';
-require_once './WPDesk/Notice/PermanentDismissibleNotice.php';
-require_once './WPDesk/Notice/Factory.php';
-require_once './WPDesk/Notice/functions.php';
