978936294c0e12383d69029dd1056d1a09417a18
Grzegorz Rola <grola@seostudio.pl>
2019-01-06 12:20:36 +0100

    Bugfix - init file

--- init.php
+++ init.php
@@ -1,18 +1,18 @@
 <?php
 
-require_once './vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 if (!class_exists('\WPDesk\Notice\AjaxHandler')) {
-    require_once './WPDesk/Notice/AjaxHandler.php';
+    require_once 'src/WPDesk/Notice/AjaxHandler.php';
 }
 if (!class_exists('\WPDesk\Notice\Notice')) {
-    require_once './WPDesk/Notice/Notice.php';
+    require_once 'src/WPDesk/Notice/Notice.php';
 }
 if (!class_exists('\WPDesk\Notice\PermanentDismissibleNotice')) {
-    require_once './WPDesk/Notice/PermanentDismissibleNotice.php';
+    require_once 'src/WPDesk/Notice/PermanentDismissibleNotice.php';
 }
 if (!class_exists('\WPDesk\Notice\Factory')) {
-    require_once './WPDesk/Notice/Factory.php';
+    require_once 'src/WPDesk/Notice/Factory.php';
 }
-require_once './WPDesk/notice-functions.php';
+require_once 'src/WPDesk/notice-functions.php';
 
