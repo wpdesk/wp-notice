9e4648900cf849fc8317ed3571f3824c79c8fc92
Grzegorz Rola <grola@seostudio.pl>
2019-01-14 11:23:51 +0100

    Code review

--- init.php
+++ init.php
@@ -1,18 +1,17 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 if (!class_exists('\WPDesk\Notice\AjaxHandler')) {
-    require_once 'src/WPDesk/Notice/AjaxHandler.php';
+    require_once __DIR__ . '/src/WPDesk/Notice/AjaxHandler.php';
 }
 if (!class_exists('\WPDesk\Notice\Notice')) {
-    require_once 'src/WPDesk/Notice/Notice.php';
+    require_once __DIR__ . 'src/WPDesk/Notice/Notice.php';
 }
 if (!class_exists('\WPDesk\Notice\PermanentDismissibleNotice')) {
-    require_once 'src/WPDesk/Notice/PermanentDismissibleNotice.php';
+    require_once __DIR__ . '/src/WPDesk/Notice/PermanentDismissibleNotice.php';
 }
 if (!class_exists('\WPDesk\Notice\Factory')) {
-    require_once 'src/WPDesk/Notice/Factory.php';
+    require_once __DIR__ . '/src/WPDesk/Notice/Factory.php';
 }
-require_once 'src/WPDesk/notice-functions.php';
-
+require_once __DIR__ . '/src/WPDesk/notice-functions.php';
